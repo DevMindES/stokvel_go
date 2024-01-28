dc81a2fed9ca2af1539f759c99be33e69a928f5d
Developer Delisa <delisamav3@gmail.com>
2024-01-28 22:15:40 +0200

    Edited files.

--- lib/pages/navigation/lend_centre.dart
+++ lib/pages/navigation/lend_centre.dart
@@ -10,12 +10,217 @@ class LendCentre extends StatefulWidget {
 class _LendCentreState extends State<LendCentre> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Center(
-          child: Text('lend'),
-        )
-      ],
-    );;
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          BorrowingInfoCard(),
+
+          BorrowingList(),
+          UnderReviewSection(),
+          GetCashButton(),
+        ],
+      ),
+    );
+  }
+}
+
+
+class BorrowingInfoCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: Card(
+        elevation: 4,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              BorrowingInfoRow(
+                icon: Icons.currency_exchange,
+                label: 'Total Borrowing',
+                value: 'R2 000',
+              ),
+              SizedBox(height: 14), // Added space for better separation
+              BorrowingInfoRow(
+                icon: Icons.trending_up,
+                label: 'Interest Accrued',
+                value: 'R 500',
+                valueColor: Colors.red,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class BorrowingInfoRow extends StatelessWidget {
+  final IconData icon;
+  final String label;
+  final String value;
+  final Color? valueColor;
+
+  const BorrowingInfoRow({
+    Key? key,
+    required this.icon,
+    required this.label,
+    required this.value,
+    this.valueColor,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 16.0),
+      child: Row(
+        children: [
+          Icon(icon, size: 24),
+          SizedBox(width: 8),
+          Text(label, style: TextStyle(fontSize: 22)),
+          Spacer(),
+          Text(
+            value,
+            style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold, color: valueColor ?? Colors.black),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class BorrowingList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(height: 30),
+          Text('Borrowing per Stokvel', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+          SizedBox(height: 8),
+          BorrowingListItem(
+            stokvelName: 'Stokvel A',
+            borrowingAmount: 'R 1 500',
+            interestAmount: 'R 400',
+          ),
+          BorrowingListItem(
+            stokvelName: 'Stokvel B',
+            borrowingAmount: 'R 500',
+            interestAmount: 'R 100',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class BorrowingListItem extends StatelessWidget {
+  final String stokvelName;
+  final String borrowingAmount;
+  final String interestAmount;
+
+  const BorrowingListItem({
+    Key? key,
+    required this.stokvelName,
+    required this.borrowingAmount,
+    required this.interestAmount,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 1,
+      margin: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Row(
+          children: [
+            Text(stokvelName, style: TextStyle(fontSize: 16)),
+            Spacer(),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Text(borrowingAmount, style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+                Text(interestAmount, style: TextStyle(fontSize: 14, color: Colors.grey)),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class UnderReviewSection extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text('Under Review', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+          SizedBox(height: 8),
+          // Repeated cards for different "under review" items
+          UnderReviewCard(title: 'Stokvel C', amount: 'R 1 500', interestRate: '20 %'),
+          UnderReviewCard(title: 'Stokvel D', amount: 'R 800', interestRate: '15 %'),
+        ],
+      ),
+    );
+  }
+}
+
+class UnderReviewCard extends StatelessWidget {
+  final String title;
+  final String amount;
+  final String interestRate;
+
+  const UnderReviewCard({
+    Key? key,
+    required this.title,
+    required this.amount,
+    required this.interestRate,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 1,
+      margin: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Row(
+          children: [
+            Text(title, style: TextStyle(fontSize: 16)),
+            Spacer(),
+            Text(amount, style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+            SizedBox(width: 4),
+            Text(interestRate, style: TextStyle(fontSize: 14, color: Colors.grey)),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class GetCashButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 24.0),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          minimumSize: Size(double.infinity, 50), // double.infinity is the width and 50 is the height
+          primary: Colors.blue, // Button color
+          onPrimary: Colors.white, // Text color
+        ),
+        onPressed: () {},
+        child: Text('Get cash', style: TextStyle(fontSize: 18)),
+      ),
+    );
   }
 }
--- lib/pages/navigation/notifications.dart
+++ lib/pages/navigation/notifications.dart
@@ -8,14 +8,101 @@ class Notifications extends StatefulWidget {
 }
 
 class _NotificationsState extends State<Notifications> {
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Center(
-          child: Text('notifications'),
-        )
-      ],
-    );;
+    return SingleChildScrollView(
+      padding: EdgeInsets.all(16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text('Today', style: TextStyle(color: Colors.black, fontSize: 18)),
+          SizedBox(height: 8),
+          NotificationTile(
+            icon: Icons.check_circle_outline,
+            text: 'Loan Approved - R 1200',
+            backgroundColor: Colors.blue,
+          ),
+          NotificationTile(
+            icon: Icons.people_outline,
+            text: 'Contribution window closes in 2 days',
+            backgroundColor: Colors.white,
+          ),
+          NotificationTile(
+            icon: Icons.remove_circle_outline,
+            text: 'Monthly Contribution + R 400',
+            backgroundColor: Colors.blue,
+          ),
+          NotificationTile(
+            icon: Icons.check,
+            text: 'Change of address successfully',
+            backgroundColor: Colors.white,
+          ),
+          // ... Add the other NotificationTile widgets here for 'Today'
+          SizedBox(height: 16),
+          Text('Last Week', style: TextStyle(color: Colors.black, fontSize: 18)),
+          SizedBox(height: 8),
+          NotificationTile(
+            icon: Icons.cancel_outlined,
+            text: 'Loan Application Declined',
+            backgroundColor: Colors.white,
+          ),
+          NotificationTile(
+            icon: Icons.people_outline,
+            text: 'Unpaid contribution for September.',
+            backgroundColor: Colors.white,
+          ),
+          NotificationTile(
+            icon: Icons.campaign_outlined,
+            text: "We're launching a new Investment vehicle in September. Be on the lookout!",
+            backgroundColor: Colors.white,
+          ),
+          NotificationTile(
+            icon: Icons.people_outline,
+            text: 'Initial Contributions due on 01/01/2024',
+            backgroundColor: Colors.white,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class NotificationTile extends StatelessWidget {
+  final IconData icon;
+  final String text;
+  final Color backgroundColor;
+
+  const NotificationTile({
+    Key? key,
+    required this.icon,
+    required this.text,
+    required this.backgroundColor,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 4),
+      padding: EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: backgroundColor,
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Row(
+        children: [
+          Icon(icon, color: backgroundColor == Colors.white ? Colors.black : Colors.white),
+          SizedBox(width: 16),
+          Expanded(
+            child: Text(
+              text,
+              style: TextStyle(
+                color: backgroundColor == Colors.white ? Colors.black : Colors.white,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
